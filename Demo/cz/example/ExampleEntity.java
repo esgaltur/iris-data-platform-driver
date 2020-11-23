10b6d34995e4aeb2619473ceda181031781f8fba
Dmitriy Sosnovich /Dmitrij Sosnovič/Дмитрий Соснович <dmitriy@sosnovich.com>
2020-11-24 00:49:13 +0100

    init

---
+++ Demo/cz/example/ExampleEntity.java
@@ -0,0 +1,27 @@
+/*
+ 
+*/
+
+package cz.esgaltur.example
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@MappedSuperclass
+public class SomeTable  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SomeGener")
+    @SequenceGenerator(name = "SomeGener", sequenceName = "SomeSegName", allocationSize = 1)
+    @Column(name = "PrimaryKey", nullable = false, unique = true)
+    private Long id;
+  
+
+}
+
